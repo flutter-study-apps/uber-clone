815656c15dd75f5e0d951cda8ac07896471bd128
ialexies <ialexies@gmail.com>
2019-11-06 12:11:34 +0800

    with working google map

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,10 +15,27 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: Map(),
     );
+  }
+}
+
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children: <Widget>[
+          GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(14.8386, 120.2842), zoom: 17.0),
+          )
+        ],
+      );
   }
 }
