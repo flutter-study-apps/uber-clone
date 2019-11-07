f391f56a0c810634284e938e1eecb21b3838f068
ialexies <ialexies@gmail.com>
2019-11-08 01:52:20 +0800

    with destination and poly

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -128,10 +128,11 @@ class _MapState extends State<Map> {
                   ),
                   child: TextField(
                     cursorColor: Colors.black,
-                    // controller: appState.destinationController,  
+                    controller: destinationControler,
                     textInputAction: TextInputAction.go,
                     onSubmitted: (value) {
                       // appState.sendRequest(value);
+                      sendRequest(value);
                     },
                     decoration: InputDecoration(
                       icon: Container(
@@ -175,65 +176,98 @@ class _MapState extends State<Map> {
         });
       }   
     
-    void _onAddMarkerPressed() {
+    void _addMarker(LatLng location, String address) {
       setState(() {
         _markers.add(
           Marker(
             markerId:MarkerId(_lastPosition.toString()),
             position: _lastPosition,
             infoWindow: InfoWindow(
-              title: "Remember Here",
-              snippet: "good place",
+              title: "Address",
+              snippet: "Go Here",
             ),
             icon: BitmapDescriptor.defaultMarker, //the actual design of the pin in the map
           ),
         ); 
       });
     }
-    
-      // !DECODE POLY
-      List _decodePoly(String poly) {
-        var list = poly.codeUnits;
-        var lList = new List();
-        int index = 0;
-        int len = poly.length;
-        int c = 0;
-      // repeating until all attributes are decoded
-        do {
-          var shift = 0;
-          int result = 0;
-    
-          // for decoding value of one attribute
-          do {
-            c = list[index] - 63;
-            result |= (c & 0x1F) << (shift * 5);
-            index++;
-            shift++;
-          } while (c >= 32);
-          /* if value is negetive then bitwise not the value */
-          if (result & 1 == 1) {
-            result = ~result;
-          }
-          var result1 = (result >> 1) * 0.00001;
-          lList.add(result1);
-        } while (index < len);
-    
-      /*adding to previous value as done in encoding */
-        for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
-    
-        print(lList.toString());
-    
-        return lList;
-      }
-    
-      void _getUserLocation() async{
-        Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-        List<Placemark> placemark =  await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude); 
-        setState(() {
-         _initialPosition=LatLng(position.latitude, position.longitude); 
-         locationController.text =placemark[0].name;
-        });
-      }
 
+    // /add the polylines
+    void createRoute(String encodedPoly){
+      setState(() {
+        _polyLines.add(Polyline(
+          polylineId: PolylineId(_lastPosition.toString()),
+          width: 10,
+          points: convertToLatLng(decodePoly(encodedPoly)),
+          color: Colors.black),
+        );
+      });
+    }
+
+    //this method will convert list of doubles into  latlng
+    List<LatLng> convertToLatLng(List points){
+      List<LatLng> result = <LatLng> [];
+      for (int i = 0; i<points.length; i++){
+        if (i%2 ==0){
+          result.add(LatLng(points[i-1], points[i]));
+        }
+      }
+      return result; 
+    }
+
+      
+    // !DECODE POLY
+    List decodePoly(String poly) {
+      var list = poly.codeUnits;
+      var lList = new List();
+      int index = 0;
+      int len = poly.length;
+      int c = 0;
+    // repeating until all attributes are decoded
+      do {
+        var shift = 0;
+        int result = 0;
   
+        // for decoding value of one attribute
+        do {
+          c = list[index] - 63;
+          result |= (c & 0x1F) << (shift * 5);
+          index++;
+          shift++;
+        } while (c >= 32);
+        /* if value is negetive then bitwise not the value */
+        if (result & 1 == 1) {
+          result = ~result;
+        }
+        var result1 = (result >> 1) * 0.00001;
+        lList.add(result1);
+      } while (index < len);
+  
+    /*adding to previous value as done in encoding */
+      for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+  
+      print(lList.toString());
+  
+      return lList;
+    }
+  
+    void _getUserLocation() async{
+      Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      List<Placemark> placemark =  await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude); 
+      setState(() {
+        _initialPosition=LatLng(position.latitude, position.longitude); 
+        locationController.text =placemark[0].name;
+      });
+    }
+    // When the user type something in the textbox it will show the placemark 
+    void sendRequest(String intendedLocation)async{
+      List<Placemark> placemark = await Geolocator().placemarkFromAddress(intendedLocation); 
+      double latitude = placemark[0].position.latitude;
+      double longitude = placemark[0].position.longitude;
+      LatLng destination = LatLng(latitude, longitude);
+      _addMarker(destination, intendedLocation); 
+      String route = await _googleMapServices.getRouteCoordinate(_initialPosition, destination);
+      createRoute(route);
+    }
+
 }
